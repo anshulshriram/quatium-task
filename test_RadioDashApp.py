633aa2462dcdf4a3799f0afffbd26e46085e5b47
AnshulSExeterUni <anshulshriram@gmail.com>
2024-09-12 07:31:25 +0100

    task 5

--- test_RadioDashApp.py
+++ test_RadioDashApp.py
@@ -4,18 +4,19 @@ import RadioDashApp as app
 
 # Test for header presence
 def test_header_is_present(dash_duo):
-    dash_duo.start_server(app, timeout=60)
-    header = dash_duo.find_element("H1")
+    dash_duo.start_server(app)
+    header = dash_duo.find_element("h1")
     assert header is not None
+    assert "Soul Foods Sales Analysis" in header.text
 
 # Test for graph presence
 def test_graph_is_present(dash_duo):
-    dash_duo.start_server(app, timeout=60)
-    graph = dash_duo.find_element("graph-1")
+    dash_duo.start_server(app)
+    graph = dash_duo.find_element("#graph-1")
     assert graph is not None
 
 # Test for radio items presence
 def test_radio_items_is_present(dash_duo):
-    dash_duo.start_server(app, timeout=60)
-    radio = dash_duo.find_elements("region-selector")
-    assert radio is not None 
+    dash_duo.start_server(app)
+    radio = dash_duo.find_element("#region-selector")
+    assert radio is not None
